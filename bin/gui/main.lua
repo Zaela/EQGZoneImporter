ba3e4fd919c004b68badfc4634c0c2cc43515555
Xackery <xackery@gmail.com>
2021-12-05 17:37:23 -0800

    Updated version to 1.3

--- bin/gui/main.lua
+++ bin/gui/main.lua
@@ -38,7 +38,7 @@ local displays = {
 require "gui/loader"
 require "gui/s3d"
 
-local title = "EQG Zone Importer v1.2"
+local title = "EQG Zone Importer v1.3"
 local window
 local tabs = iup.tabs{padding = "5x5"}
 
